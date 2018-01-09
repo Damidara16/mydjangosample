f7aaeb1b094957478903c21bf0f846ff01575a66
simeon ajala <herohawk5@gmail.com>
2018-01-08 19:48:38 -0600

    install the app in settings

--- proj_1/urls.py
+++ proj_1/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-app_name = "proj_1"
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.homeView.as_view(), name="index"),
+    url(r'^home/', views.index, name='home')
 ]
--- proj_1/views.py
+++ proj_1/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 def index(request):
-    return render(request, "proj_1/index.html")
+    return render(request, 'proj_1/home.html')
+
+class homeView(TemplateView):
+
+    template_name = "proj_1/index.html"
